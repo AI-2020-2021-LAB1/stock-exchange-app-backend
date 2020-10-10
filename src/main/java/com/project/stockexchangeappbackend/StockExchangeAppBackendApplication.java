21f1343eeac34309094656f953b5e215bd59691b
Paweł Kocan <pkocan@onet.pl>
2020-10-10 15:24:26 +0200

    Created the project with the Spring Initializr, initial commit

---
+++ src/main/java/com/project/stockexchangeappbackend/StockExchangeAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.stockexchangeappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangeAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockExchangeAppBackendApplication.class, args);
+	}
+
+}
