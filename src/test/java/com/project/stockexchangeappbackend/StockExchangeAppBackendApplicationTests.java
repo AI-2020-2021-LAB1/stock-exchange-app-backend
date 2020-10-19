df44f62dd399362ba0b80e3ab7a27b4df886db2b
kcymerys <karol.cymerys@gmail.com>
2020-10-19 17:27:41 +0200

    Added Dockerfile

--- src/test/java/com/project/stockexchangeappbackend/StockExchangeAppBackendApplicationTests.java
+++ src/test/java/com/project/stockexchangeappbackend/StockExchangeAppBackendApplicationTests.java
@@ -1,12 +1,10 @@
 package com.project.stockexchangeappbackend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StockExchangeAppBackendApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
