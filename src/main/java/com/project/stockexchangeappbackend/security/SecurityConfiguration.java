629c98ca86c04145c8ad738727f2235e6ee12f55
kcymerys <karol.cymerys@gmail.com>
2020-10-26 16:59:12 +0100

    Resolved CORS issue (Authorization)

--- src/main/java/com/project/stockexchangeappbackend/security/SecurityConfiguration.java
+++ src/main/java/com/project/stockexchangeappbackend/security/SecurityConfiguration.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -36,6 +38,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomDetailsService(userRepository);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().antMatchers(HttpMethod.OPTIONS);
     }
 
 }
