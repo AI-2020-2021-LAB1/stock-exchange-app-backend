c0d0e9a0fce772c119a27791573732b31584b5a3
kcymerys <karol.cymerys@gmail.com>
2020-12-19 16:19:39 +0100

    Fixed bug application user's changes after his re-login

--- src/main/java/com/project/stockexchangeappbackend/security/ResourceServerConfiguration.java
+++ src/main/java/com/project/stockexchangeappbackend/security/ResourceServerConfiguration.java
@@ -1,15 +1,24 @@
 package com.project.stockexchangeappbackend.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 
 @Configuration
 @EnableResourceServer
+@AllArgsConstructor
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    private final BannedAccessTokens bannedAccessTokens;
+    private final ObjectMapper objectMapper;
+    private final Environment env;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -19,6 +28,10 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                 .antMatchers("/api/**").permitAll()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterAfter(
+                new CustomFilterSecurityInterceptor(bannedAccessTokens, objectMapper, env),
+                FilterSecurityInterceptor.class);
     }
 
 
